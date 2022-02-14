31b65b4ee72b5470387dcef838779a74846355b6
DJrOf <danieljrofosu@gmail.com>
2022-02-14 15:49:14 +0100

    Basic layout

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <input type="text"> <button>Cerca</button>
+  </header>
+</template>
+
+<script>
+export default {
+name: 'Header', 
+}
+</script>
+
+<style>
+
+</style>
